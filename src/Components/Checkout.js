14346178e051db50ef9657ac358de8e35e524255
Sujeet Raj Chaudhari <61381167+Sujeet-Raj-Chaudhari@users.noreply.github.com>
2022-07-11 09:03:50 +0530

    UserName On Pages

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -5,7 +5,7 @@ import CheckoutProduct from "./CheckoutProduct";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -15,20 +15,23 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
-      </div>
-      <div>
-        <h2 className="checkout_title">Your Shopping Basket</h2>
 
-        {basket.map((item) => (
-          <CheckoutProduct
-            id={item.id}
-            title={item.title}
-            image={item.image}
-            price={item.price}
-            rating={item.rating}
-          />
-        ))}
+        <div>
+          <h3>Hello, {user ? user.email : "Guest"}</h3>
+          <h2 className="checkout_title">Your Shopping Basket</h2>
+
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+        </div>
       </div>
+
       <div className="checkout_right"></div>
       <Subtotal />
     </div>
